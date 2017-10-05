834b804d6797dd415d0c15126904b5119ccaa638
HaoCheng Yang <cabbagehao@qq.com>
2017-10-05 21:10:30 +0800

    用分治法实现最小和序列并简单验证

--- A02/MinSubarraySum/binary_solution.py
+++ A02/MinSubarraySum/binary_solution.py
@@ -4,7 +4,66 @@
         1. 在左边部分
         2. 在右边部分
         3. 在两部分都有
-  2. 前2种情况直接递归，第3种情况需要特殊处理：
-        1. 前半部分包含最后一个的最小和序列 + 后半部分包含第一个的最小和序列
-        2. 这个最小和序列比较好求，只需要遍历到一个大于0的数停止就好了
+  2. 前2种情况直接递归，第3种情况的最小和为： 前半部分包含最后一个的最小和序列 + 后半部分包含第一个的最小和序列
+  3. 因此，函数里计算3个sum，比较这3个sum的大小即可。
+        1. 左右2个sum通过递归计算。 每次递归都进行这样的过程，直到分解为单个元素的数组时返回。
+        2. 另一个sum通过单独的函数计算
 ”“”
+
+
+def find_min_sum_cross_subarray(left, right):
+    
+    left_sum = 100000
+    this_sum = 0
+    for i in range(len(left) - 1, -1, -1):
+        this_sum += left[i]
+        if this_sum < left_sum:
+            left_sum = this_sum
+
+
+    right_sum = 100000
+    this_sum = 0
+    for i in range(len(right) - 1, -1, -1):
+        this_sum += right[i]
+        if this_sum < right_sum:
+            right_sum = this_sum
+
+    return left_sum + right_sum
+
+def find_min_sum_subarray(nums, case_id):
+    if not nums:
+        return None
+    if len(nums) == 1:
+        return nums[0]
+
+    mid = len(nums) / 2
+    left = nums[:mid]
+    right = nums[mid:]
+    left_sum = find_min_sum_subarray(left, case_id)
+    right_sum = find_min_sum_subarray(right, case_id)
+    cross_sum = find_min_sum_cross_subarray(left, right)
+
+    if left_sum <= right_sum and left_sum <= cross_sum:
+        return left_sum
+    elif right_sum <= left_sum and right_sum <= cross_sum:
+        return right_sum
+    else:
+        return cross_sum
+
+
+# [[nums, target, expect]]
+TestCase = [    [[1], 1],
+                [[1, 2, 3], 1],
+                [[1,2,-1,-2], -3],                
+            ]           
+
+
+
+
+for i in range(len(TestCase)):
+    testcase = TestCase[i]
+    nums = testcase[0]
+    expect = testcase[1]
+    ret = find_min_sum_subarray(nums, i)
+    if abs(expect - ret) > 0.0001:
+        print 'Test Case ', i, ' failed.', expect, ret
