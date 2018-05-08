da32154953d82c3e6ee4f3a8743037713d9ab67b
cabbagehao <cabbagehao@qq.com>
2018-05-08 11:41:11 +0800

    Deleted all leetcode files

--- 009 K-diff Pairs/Top1_solution.py
+++
@@ -1,7 +0,0 @@
-# 很精妙的解法。 使用到了collections 模块。
-# 比我的解法提高20% 效率。
-
-def findPairs(nums, k):
-    return  len(set(nums) & {n+k for n in nums}) if k > 0  \
-            else sum(v > 1 for v in collections.Counter(nums).values()) if k == 0  \
-            else 0
